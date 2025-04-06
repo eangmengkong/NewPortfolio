bf63334e9b017efc4b9e398105c7a3d2bf805cac
Eang Mengkong <eangmengkong21@gmail.com>
2025-04-06 14:47:16 +0700

    edit file

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
-  base: '/',
+  base: './',
 });
