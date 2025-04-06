d8ca1ae0e9ec989803760ae5cf11ea9027faf281
Eang Mengkong <eangmengkong21@gmail.com>
2025-04-06 13:51:18 +0700

    edit file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
   plugins: [
     react(),
@@ -19,4 +19,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  base: '/',
+});
